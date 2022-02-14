e8035562e88d064b0b23767b1420033f8cae76ed
Lukas <lukas@open@ivf.com>
2022-02-14 19:46:09 +0100

    powered by

--- pages/index.js
+++ pages/index.js
@@ -49,11 +49,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/Lukas-DH"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by OPEN-IVF
         </a>
       </footer>
     </div>
