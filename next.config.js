c86164f6858fe6431d8df847ec9ec305e6aee3c1
Lukas <lukas@open@ivf.com>
2022-02-15 11:13:17 +0100

    html extention 2

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ module.exports = {
   ) {
     return {
       "/": { page: "/" },
-      "/services": { page: "/services.html" },
-      "/contact": { page: "/contact.html" },
+      "/services.html": { page: "/services" },
+      "/contact.html": { page: "/contact" },
     };
   },
 };
